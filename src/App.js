f269453bba6dac30cd5599a4bdd8b48be139c214
Julien Blondeau <julien@euranov.com>
2021-05-18 16:38:58 +0200

    FCL and script

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React from "react"
+import React, { useEffect } from "react"
 import {AuthCluster} from "./AuthCluster"
+import * as fcl from "@onflow/fcl"
+import * as t from "@onflow/types"
 
 function App() {
+
+  useEffect(() => {
+    async function runScript() {
+      let x = await fcl
+      .send([
+        fcl.script`
+          import Profile from 0xba1132bc08f82fe2
+    
+          pub fun main(address: Address): Profile.ReadOnly? {
+            return Profile.read(address)
+          }
+        `,
+        fcl.args([
+          fcl.arg("0xba1132bc08f82fe2", t.Address), // <-- t.Address this time :)
+        ]),
+      ])
+      .then(fcl.decode)
+    
+      console.log(x)
+    }
+
+    runScript();
+    
+  }, []);
   return (
     <div>
       <AuthCluster />
